1a4d200391f485a0fd3961bd190a2d42a890a6b7
supermin45 <1299657352@qq.com>
2017-08-08 07:13:22 +0000

    finish

---
+++ prime_number.js
@@ -0,0 +1,26 @@
+    function prime_number(num) {
+        let i = 2;
+        let arr = [];
+
+        while(i <= num){
+            if(num % i == 0){
+                arr.push(i);
+                num /= i;
+            }else{
+                i++;
+            }
+        }
+        return get_result(arr);
+    }
+    
+    function get_result(arr) {
+        let str = ' ';
+        for (let i in arr) {
+            str += arr[i];
+        }
+        return str;
+    }
+    
+    let num = 90;
+    let string = prime_number(num);
+    console.log(num + '=' + string);

597d5c4bcbed6c285a51afecfb264a2b6a6c5bb3
supermin45 <1299657352@qq.com>
2017-08-11 14:34:35 +0800

    'finished'

--- test_0811.js
+++ test_0811.js
@@ -1,5 +1,6 @@
 /**
  * Created by Administrator on 2017/8/11.
+ * 题目：给出链表 1->2->3->3->4->5->3, 和 val = 3, 你需要返回删除3之后的链表：1->2->4->5。g
  */
 function chain(str, cal) {
     let arr = splitStr(str, cal);

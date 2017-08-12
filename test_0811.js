27c94b3215cad15ef670da9203e569470e5ef299
supermin45 <1299657352@qq.com>
2017-08-12 14:19:45 +0800

    'NarcissisticNumber'

--- test_0811.js
+++ test_0811.js
@@ -3,15 +3,17 @@
  * 题目：给出链表 1->2->3->3->4->5->3, 和 val = 3, 你需要返回删除3之后的链表：1->2->4->5。g
  */
 function fromChainDeleteVal(str, val) {
+
     let result = [];
     var arr = str.split('->');
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] !== (''+val)) {
+        if (arr[i] !== ('' + val)) {
             result.push(arr[i]);
         }
     }
-    result =  result.join('->');
+    result = result.join('->');
     return result;
+
 }
 let str = '1->2->3->3->4->5->3';
 let val = 3;
---
+++ test_0812.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Administrator on 2017/8/12.
+ */
+function printNarcissistic() {
+    let arr = [];
+    for (let i = 100; i < 999; i++) {
+        let hundredNumber = Math.floor(i / 100);
+        let tenNumber = Math.floor(i % 100 / 10);
+        let bitNumber = i % 10;
+        let a = Math.pow(hundredNumber,3) + Math.pow(tenNumber,3) + Math.pow(bitNumber,3);
+        if (i === a) {
+            arr.push(i);
+        }
+    }
+    return arr;
+}
+console.log('所有的水仙花数为：' + printNarcissistic());

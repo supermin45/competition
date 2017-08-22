674aef972e918a594e7777f3b56eb447302a7a3a
supermin45 <1299657352@qq.com>
2017-08-22 14:46:36 +0800

    intersection

--- test_0822.js
+++ test_0822.js
@@ -6,11 +6,16 @@
  例如：nums1 = [1, 2, 2, 1], nums2 = [2, 2], 返回 [2, 2].
  */
 function findIntersection (nums1, nums2) {
-    return nums1.filter(e => e === nums2[0] || e === nums2[nums2.length -1]);
+    let result = nums1.filter(e => nums2.includes(e) === true);
+    if (result.length === 2) {
+        return result;
+    } else {
+        return result.filter((e, index) => result.lastIndexOf(e) === index);
+    }
 }
 const nums1 = [1, 2, 2, 1];
 const nums2 = [2, 2];
-const nums3 = [1, 2, 3, 4];
-const nums4 = [2, 3, 4];
+const nums3 = [1, 2, 2, 3, 4];
+const nums4 = [2, 3, 3, 4];
 console.log(`[${nums1}]与[${nums2}]的交集为[${findIntersection (nums1, nums2)}]`);
 console.log(`[${nums3}]与[${nums4}]的交集为[${findIntersection (nums3, nums4)}]`);
